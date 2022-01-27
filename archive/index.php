30e57d5f80001f3254a26df3e515b5a64a7bb704
Dan Rosser <danoli3@gmail.com>
2022-01-27 23:41:42 +1100

    jQuery Security
    (cherry picked from commit c8409504a830523b83f296ec248ff9d35f8ace19)

--- archive/index.php
+++ archive/index.php
@@ -39,7 +39,9 @@ if( !file_exists('data.xml') )
 			<div class="uk-grid">
 			</div>
 		</div>
-		<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"   
+			integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+			crossorigin="anonymous"></script>
 		<script type="text/javascript">
 			$(function() {
 				$(".uk-grid").load("create.php?s=installation");
